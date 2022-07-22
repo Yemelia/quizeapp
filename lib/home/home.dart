266f843ed1e1dc3477deacea1160f133d9f88a84
Yemelia <yemelianovn@tut.by>
2022-07-23 00:33:13 +0200

    added routes and theme

---
+++ lib/home/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          child: Text(
+            'about',
+            style: Theme.of(context).textTheme.button,
+          ),
+          onPressed: () => Navigator.pushNamed(context, '/about'),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 // Import the firebase_core plugin
 import 'package:firebase_core/firebase_core.dart';
+import 'package:quizapp/routes.dart';
+import 'package:quizapp/theme.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -31,14 +33,17 @@ class _AppState extends State<App> {
       // Initialize FlutterFire:
       future: _initialization,
       builder: (context, snapshot) {
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            routes:  appRoutes,
+            theme: appTheme,
+          );
+        }
+
         // Check for errors
         if (snapshot.hasError) {
           return const Text('error', textDirection: TextDirection.ltr);
-        }
-
-        // Once complete, show your application
-        if (snapshot.connectionState == ConnectionState.done) {
-          return const MaterialApp();
         }
 
         // Otherwise, show something whilst waiting for initialization to complete
